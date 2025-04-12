3971bec5ea95cc5fcc2ede57d6ee1e28470314d4
Efim Nushtaev <enushtaev@alfabank.ru>
2025-04-13 00:33:55 +0300

    Remove unused ESP32 project files and configurations for esp32-blink and esp32-ds18b20

--- esp32-ds18b20/src/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// here need add your wifi ssid, password and clientId
-#define WIFI_SSID "WIFI_SSID"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
-#define CLIENT_ID "CLIENT_ID"
-
-#endif // CONFIG_H
